2ba5a8535cb1ecbaece5d90313347bac03836751
Phong Vu <phongvu@feng.local>
2019-06-04 18:54:08 +0700

    remove google key

--- src/constant/config.js
+++ src/constant/config.js
@@ -5,9 +5,6 @@ const AppConfig = {
   apiDomain,
   isDebug: apiDomain !== liveDomain,
   staticImages: `${staticFile}/images/`,
-  googleApiKey:
-    (window && window.config && window.config.googleMapsKey) ||
-    'AIzaSyBbK45238J7Kgp6YIC3grq92sQN8gJq1W4',
 };
 
 export default AppConfig;
